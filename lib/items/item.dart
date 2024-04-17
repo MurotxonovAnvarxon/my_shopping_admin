5c53de604c6bf6b4ffedb7d310c50448b323f385
umidjonakramov699 <umidjonakramov699@gmail.com>
2024-04-17 16:15:29 +0500

    get

--- lib/items/item.dart
+++ lib/items/item.dart
@@ -27,222 +27,222 @@ class Item {
   static final formatter =
       NumberFormat.currency(locale: 'id_ID', symbol: "So'm ");
   
-  static List<Item>  dummyItems=getImageInformationList("storagePath") as List<Item>;
+  // static List<Item>  dummyItems=getImageInformationList("storagePath");
   // >[
 
     // Item(id: get, name: name, description: description, price: price, inStock: inStock, imageUrl: imageUrl)
   // ];
 
-  // static List<Item> get dummyItems => [
-  //       Item(
-  //           id: "1",
-  //           name: "Stullar",
-  //           description: '20 talik toplam',
-  //           price: 200000,
-  //           inStock: true,
-  //           imageUrl:
-  //               'https://frankfurt.apollo.olxcdn.com/v1/files/yaphoxo79ba73-UZ/image;s=1264x547'),
-  //       Item(
-  //           id: "2",
-  //           name: "Stullar",
-  //           description: '8 talik toplam',
-  //           price: 1500000,
-  //           inStock: true,
-  //           imageUrl:
-  //               'https://tashmebel.uz/wp-content/uploads/2022/11/photo_2022-05-28_01-32-51.jpg'),
-  //       Item(
-  //           id: "3",
-  //           name: "Decaration",
-  //           description: 'More magical than ever.',
-  //           price: 20000,
-  //           inStock: true,
-  //           imageUrl:
-  //               'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIOhbGLCziCi6-P40I7RHB31-JoIuZo-3-Mmda2LrGrw&s'),
-  //       Item(
-  //           id: "4",
-  //           name: "Decaration",
-  //           description: 'More magical than ever.',
-  //           price: 200000,
-  //           inStock: true,
-  //           imageUrl:
-  //               'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVYko2a7vQoaOFL4Cx-b8a-sPkW7w18kYrkrPbipiNJg&s'),
-  //       Item(
-  //           id: "5",
-  //           name: "Decaration",
-  //           description: 'More magical than ever.',
-  //           price: 20000,
-  //           inStock: true,
-  //           imageUrl:
-  //               'https://m.media-amazon.com/images/I/81IRT5qqW0L._AC_UF1000,1000_QL80_.jpg'),
-  //       Item(
-  //           id: "6",
-  //           name: "Decaration",
-  //           description: 'More magical than ever.',
-  //           price: 20000,
-  //           inStock: true,
-  //           imageUrl:
-  //               'https://img.staticmb.com/mbcontent/images/uploads/2022/5/wedding%20stage%20decoration.jpg'),
-  //       Item(
-  //           id: "7",
-  //           name: "Decaration",
-  //           description: 'More magical than ever.',
-  //           price: 2000,
-  //           inStock: true,
-  //           imageUrl:
-  //               'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1uYhoR8ozyv4nHN2zClzuIhI8NeXSQF03D4BwlRSzpw&s'),
-  //       Item(
-  //           id: "8",
-  //           name: "Decaration",
-  //           description: 'More magical than ever.',
-  //           price: 2000,
-  //           inStock: true,
-  //           imageUrl:
-  //               'https://img.staticmb.com/mbcontent/images/uploads/2022/5/wedding%20stage%20decoration.jpg'),
-  //       Item(
-  //           id: "9",
-  //           name: "Decaration",
-  //           description: 'More magical than ever.',
-  //           price: 2000,
-  //           inStock: true,
-  //           imageUrl:
-  //               'https://rukminim2.flixcart.com/image/850/1000/xif0q/decoration/w/n/o/2000-large-0-75-8970429098-balloon-decoration-arch-stand-samanth-original-imagjwbtzwhyxfha.jpeg?q=90&crop=true'),
-  //       Item(
-  //           id: "10",
-  //           name: "Decaration",
-  //           description: 'More magical than ever.',
-  //           price: 2000,
-  //           inStock: true,
-  //           imageUrl:
-  //               'https://d2j6dbq0eux0bg.cloudfront.net/images/7533597/3712335336.jpg'),
-  //       Item(
-  //           id: "11",
-  //           name: "Decaration",
-  //           description: 'More magical than ever.',
-  //           price: 2000,
-  //           inStock: true,
-  //           imageUrl:
-  //               'https://d2j6dbq0eux0bg.cloudfront.net/images/7533597/3712335336.jpg'),
-  //       Item(
-  //           id: "12",
-  //           name: "Decaration",
-  //           description: 'More magical than ever.',
-  //           price: 2000,
-  //           inStock: true,
-  //           imageUrl:
-  //               'https://d2j6dbq0eux0bg.cloudfront.net/images/7533597/3712335336.jpg'),
-  //       Item(
-  //           id: "13",
-  //           name: "Decaration",
-  //           description: 'More magical than ever.',
-  //           price: 2000,
-  //           inStock: true,
-  //           imageUrl:
-  //               'https://d2j6dbq0eux0bg.cloudfront.net/images/7533597/3712335336.jpg'),
-  //       Item(
-  //           id: "14",
-  //           name: "Decaration",
-  //           description: 'More magical than ever.',
-  //           price: 2000,
-  //           inStock: true,
-  //           imageUrl:
-  //               'https://d2j6dbq0eux0bg.cloudfront.net/images/7533597/3712335336.jpg'),
-  //       Item(
-  //           id: "15",
-  //           name: "Decaration",
-  //           description: 'More magical than ever.',
-  //           price: 2000,
-  //           inStock: true,
-  //           imageUrl:
-  //               'https://d2j6dbq0eux0bg.cloudfront.net/images/7533597/3712335336.jpg'),
-  //       Item(
-  //           id: "16",
-  //           name: "Decaration",
-  //           description: 'More magical than ever.',
-  //           price: 2000,
-  //           inStock: true,
-  //           imageUrl:
-  //               'https://d2j6dbq0eux0bg.cloudfront.net/images/7533597/3712335336.jpg'),
-  //       Item(
-  //           id: "17",
-  //           name: "Decaration",
-  //           description: 'More magical than ever.',
-  //           price: 2000,
-  //           inStock: true,
-  //           imageUrl:
-  //               'https://d2j6dbq0eux0bg.cloudfront.net/images/7533597/3712335336.jpg'),
-  //       Item(
-  //           id: "18",
-  //           name: "Decaration",
-  //           description: 'More magical than ever.',
-  //           price: 2000,
-  //           inStock: true,
-  //           imageUrl:
-  //               'https://d2j6dbq0eux0bg.cloudfront.net/images/7533597/3712335336.jpg'),
-  //       Item(
-  //           id: "19",
-  //           name: "Decaration",
-  //           description: 'More magical than ever.',
-  //           price: 2000,
-  //           inStock: true,
-  //           imageUrl:
-  //               'https://d2j6dbq0eux0bg.cloudfront.net/images/7533597/3712335336.jpg'),
-  //       Item(
-  //           id: "20",
-  //           name: "Decaration",
-  //           description: 'More magical than ever.',
-  //           price: 2000,
-  //           inStock: true,
-  //           imageUrl:
-  //               'https://d2j6dbq0eux0bg.cloudfront.net/images/7533597/3712335336.jpg'),
-  //       Item(
-  //           id: "21",
-  //           name: "Decaration",
-  //           description: 'More magical than ever.',
-  //           price: 2000,
-  //           inStock: true,
-  //           imageUrl:
-  //               'https://d2j6dbq0eux0bg.cloudfront.net/images/7533597/3712335336.jpg'),
-  //       Item(
-  //           id: "22",
-  //           name: "Decaration",
-  //           description: 'More magical than ever.',
-  //           price: 2000,
-  //           inStock: true,
-  //           imageUrl:
-  //               'https://d2j6dbq0eux0bg.cloudfront.net/images/7533597/3712335336.jpg'),
-  //       Item(
-  //           id: "23",
-  //           name: "Decaration",
-  //           description: 'More magical than ever.',
-  //           price: 2000,
-  //           inStock: true,
-  //           imageUrl:
-  //               'https://d2j6dbq0eux0bg.cloudfront.net/images/7533597/3712335336.jpg'),
-  //       Item(
-  //           id: "24",
-  //           name: "Decaration",
-  //           description: 'More magical than ever.',
-  //           price: 2000,
-  //           inStock: true,
-  //           imageUrl:
-  //               'https://d2j6dbq0eux0bg.cloudfront.net/images/7533597/3712335336.jpg'),
-  //       Item(
-  //           id: "25",
-  //           name: "Decaration",
-  //           description: 'More magical than ever.',
-  //           price: 2000,
-  //           inStock: true,
-  //           imageUrl:
-  //               'https://d2j6dbq0eux0bg.cloudfront.net/images/7533597/3712335336.jpg'),
-  //       Item(
-  //           id: "26",
-  //           name: "Decaration",
-  //           description: 'More magical than ever.',
-  //           price: 2000,
-  //           inStock: true,
-  //           imageUrl:
-  //               'https://d2j6dbq0eux0bg.cloudfront.net/images/7533597/3712335336.jpg'),
-  //     ];
+  static List<Item> get dummyItems => [
+        Item(
+            id: "1",
+            name: "Stullar",
+            description: '20 talik toplam',
+            price: 200000,
+            inStock: true,
+            imageUrl:
+                'https://frankfurt.apollo.olxcdn.com/v1/files/yaphoxo79ba73-UZ/image;s=1264x547'),
+        Item(
+            id: "2",
+            name: "Stullar",
+            description: '8 talik toplam',
+            price: 1500000,
+            inStock: true,
+            imageUrl:
+                'https://tashmebel.uz/wp-content/uploads/2022/11/photo_2022-05-28_01-32-51.jpg'),
+        Item(
+            id: "3",
+            name: "Decaration",
+            description: 'More magical than ever.',
+            price: 20000,
+            inStock: true,
+            imageUrl:
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIOhbGLCziCi6-P40I7RHB31-JoIuZo-3-Mmda2LrGrw&s'),
+        Item(
+            id: "4",
+            name: "Decaration",
+            description: 'More magical than ever.',
+            price: 200000,
+            inStock: true,
+            imageUrl:
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVYko2a7vQoaOFL4Cx-b8a-sPkW7w18kYrkrPbipiNJg&s'),
+        Item(
+            id: "5",
+            name: "Decaration",
+            description: 'More magical than ever.',
+            price: 20000,
+            inStock: true,
+            imageUrl:
+                'https://m.media-amazon.com/images/I/81IRT5qqW0L._AC_UF1000,1000_QL80_.jpg'),
+        Item(
+            id: "6",
+            name: "Decaration",
+            description: 'More magical than ever.',
+            price: 20000,
+            inStock: true,
+            imageUrl:
+                'https://img.staticmb.com/mbcontent/images/uploads/2022/5/wedding%20stage%20decoration.jpg'),
+        Item(
+            id: "7",
+            name: "Decaration",
+            description: 'More magical than ever.',
+            price: 2000,
+            inStock: true,
+            imageUrl:
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1uYhoR8ozyv4nHN2zClzuIhI8NeXSQF03D4BwlRSzpw&s'),
+        Item(
+            id: "8",
+            name: "Decaration",
+            description: 'More magical than ever.',
+            price: 2000,
+            inStock: true,
+            imageUrl:
+                'https://img.staticmb.com/mbcontent/images/uploads/2022/5/wedding%20stage%20decoration.jpg'),
+        Item(
+            id: "9",
+            name: "Decaration",
+            description: 'More magical than ever.',
+            price: 2000,
+            inStock: true,
+            imageUrl:
+                'https://rukminim2.flixcart.com/image/850/1000/xif0q/decoration/w/n/o/2000-large-0-75-8970429098-balloon-decoration-arch-stand-samanth-original-imagjwbtzwhyxfha.jpeg?q=90&crop=true'),
+        Item(
+            id: "10",
+            name: "Decaration",
+            description: 'More magical than ever.',
+            price: 2000,
+            inStock: true,
+            imageUrl:
+                'https://d2j6dbq0eux0bg.cloudfront.net/images/7533597/3712335336.jpg'),
+        Item(
+            id: "11",
+            name: "Decaration",
+            description: 'More magical than ever.',
+            price: 2000,
+            inStock: true,
+            imageUrl:
+                'https://d2j6dbq0eux0bg.cloudfront.net/images/7533597/3712335336.jpg'),
+        Item(
+            id: "12",
+            name: "Decaration",
+            description: 'More magical than ever.',
+            price: 2000,
+            inStock: true,
+            imageUrl:
+                'https://d2j6dbq0eux0bg.cloudfront.net/images/7533597/3712335336.jpg'),
+        Item(
+            id: "13",
+            name: "Decaration",
+            description: 'More magical than ever.',
+            price: 2000,
+            inStock: true,
+            imageUrl:
+                'https://d2j6dbq0eux0bg.cloudfront.net/images/7533597/3712335336.jpg'),
+        Item(
+            id: "14",
+            name: "Decaration",
+            description: 'More magical than ever.',
+            price: 2000,
+            inStock: true,
+            imageUrl:
+                'https://d2j6dbq0eux0bg.cloudfront.net/images/7533597/3712335336.jpg'),
+        Item(
+            id: "15",
+            name: "Decaration",
+            description: 'More magical than ever.',
+            price: 2000,
+            inStock: true,
+            imageUrl:
+                'https://d2j6dbq0eux0bg.cloudfront.net/images/7533597/3712335336.jpg'),
+        Item(
+            id: "16",
+            name: "Decaration",
+            description: 'More magical than ever.',
+            price: 2000,
+            inStock: true,
+            imageUrl:
+                'https://d2j6dbq0eux0bg.cloudfront.net/images/7533597/3712335336.jpg'),
+        Item(
+            id: "17",
+            name: "Decaration",
+            description: 'More magical than ever.',
+            price: 2000,
+            inStock: true,
+            imageUrl:
+                'https://d2j6dbq0eux0bg.cloudfront.net/images/7533597/3712335336.jpg'),
+        Item(
+            id: "18",
+            name: "Decaration",
+            description: 'More magical than ever.',
+            price: 2000,
+            inStock: true,
+            imageUrl:
+                'https://d2j6dbq0eux0bg.cloudfront.net/images/7533597/3712335336.jpg'),
+        Item(
+            id: "19",
+            name: "Decaration",
+            description: 'More magical than ever.',
+            price: 2000,
+            inStock: true,
+            imageUrl:
+                'https://d2j6dbq0eux0bg.cloudfront.net/images/7533597/3712335336.jpg'),
+        Item(
+            id: "20",
+            name: "Decaration",
+            description: 'More magical than ever.',
+            price: 2000,
+            inStock: true,
+            imageUrl:
+                'https://d2j6dbq0eux0bg.cloudfront.net/images/7533597/3712335336.jpg'),
+        Item(
+            id: "21",
+            name: "Decaration",
+            description: 'More magical than ever.',
+            price: 2000,
+            inStock: true,
+            imageUrl:
+                'https://d2j6dbq0eux0bg.cloudfront.net/images/7533597/3712335336.jpg'),
+        Item(
+            id: "22",
+            name: "Decaration",
+            description: 'More magical than ever.',
+            price: 2000,
+            inStock: true,
+            imageUrl:
+                'https://d2j6dbq0eux0bg.cloudfront.net/images/7533597/3712335336.jpg'),
+        Item(
+            id: "23",
+            name: "Decaration",
+            description: 'More magical than ever.',
+            price: 2000,
+            inStock: true,
+            imageUrl:
+                'https://d2j6dbq0eux0bg.cloudfront.net/images/7533597/3712335336.jpg'),
+        Item(
+            id: "24",
+            name: "Decaration",
+            description: 'More magical than ever.',
+            price: 2000,
+            inStock: true,
+            imageUrl:
+                'https://d2j6dbq0eux0bg.cloudfront.net/images/7533597/3712335336.jpg'),
+        Item(
+            id: "25",
+            name: "Decaration",
+            description: 'More magical than ever.',
+            price: 2000,
+            inStock: true,
+            imageUrl:
+                'https://d2j6dbq0eux0bg.cloudfront.net/images/7533597/3712335336.jpg'),
+        Item(
+            id: "26",
+            name: "Decaration",
+            description: 'More magical than ever.',
+            price: 2000,
+            inStock: true,
+            imageUrl:
+                'https://d2j6dbq0eux0bg.cloudfront.net/images/7533597/3712335336.jpg'),
+      ];
 }
 
 Future<List<Map<String, dynamic>>?> getImageInformationList(String storagePath) async {
--- lib/screens/add_prodect_screen/add_product_screen.dart
+++ lib/screens/add_prodect_screen/add_product_screen.dart
@@ -68,6 +68,104 @@ class _AddProductState extends State<AddProduct> {
     }
   }
 
+
+  Future<List<String>> getImageUrlsFromFirestore(String storagePath) async {
+    try {
+      QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+          .collection('productLast')
+          .where('storagePath', isEqualTo: storagePath)
+          .get();
+
+      List<String> imageUrlList = [];
+
+      if (querySnapshot.docs.isNotEmpty) {
+        querySnapshot.docs.forEach((DocumentSnapshot document) {
+          String imageUrl = document.get('imageUrl');
+          imageUrlList.add(imageUrl);
+        });
+
+        return imageUrlList;
+      } else {
+        print('No image URLs found for storagePath: $storagePath');
+        return [];
+      }
+    } catch (e) {
+      print('Error retrieving image URLs: $e');
+      return [];
+    }
+  }
+
+
+// Method to retrieve a list of image paths from Firebase Storage
+  Future<List<String>> getImagePathsFromStorage(String storagePath) async {
+    try {
+      List<String> imagePaths = [];
+
+      // Create a reference to the desired path in Firebase Storage
+      Reference storageRef = FirebaseStorage.instance.ref().child(storagePath);
+
+      // List all items (files and directories) within the specified path
+      ListResult listResult = await storageRef.listAll();
+
+      // Iterate through each item (file) in the list result
+      listResult.items.forEach((Reference item) {
+        // Get the full path (including parent path) of the item
+        String fullPath = item.fullPath;
+        // Add the full path to the list of image paths
+        imagePaths.add(fullPath);
+      });
+
+      return imagePaths;
+    } catch (e) {
+      print('Error retrieving image paths from Firebase Storage: $e');
+      return []; // Return an empty list in case of error
+    }
+  }
+
+  void fetchAndPrintImagePaths(String storagePath) async {
+    try {
+      List<String> imagePaths = await getImagePathsFromStorage(storagePath);
+
+      if (imagePaths.isNotEmpty) {
+        // Print each image path in the list
+        print('Image Paths:');
+        imagePaths.forEach((path) {
+          print(path);
+        });
+      } else {
+        print('No image paths found.');
+      }
+    } catch (e) {
+      print('Error fetching and printing image paths: $e');
+    }
+  }
+
+  void fetchImagePaths(String storagePath) async {
+    List<String> imagePaths = await getImagePathsFromStorage(storagePath);
+
+    if (imagePaths.isNotEmpty) {
+      // Image paths retrieved successfully
+      print('Image paths found:');
+      imagePaths.forEach((path) {
+        print(path);
+      });
+
+      // Now you can use the retrieved image paths to download or display images
+    } else {
+      // No image paths found or error occurred
+      print('No image paths found in Firebase Storage for path: $storagePath');
+    }
+  }
+
+
+  @override
+  void initState() {
+    fetchAndPrintImagePaths('rasm/');
+    print('----------------------------------------------------');
+    print("---------+   ${getImagePathsFromStorage('rasm/').toString()}");
+    super.initState();
+  }
+
   //
   // // Function to add product to Firestore
   // Future<void> addProductToFirestore() async {
@@ -304,7 +402,7 @@ class _AddProductState extends State<AddProduct> {
                   } else if (controllerPrice.text.isEmpty) {
                     showSnackbar(context, 'Please enter a valid price');
                   } else {
-                    var idAndPath=generateUniqueId();
+                    var idAndPath = generateUniqueId();
                     uploadImageToStorageAndFirestore(
                         idAndPath,
                         idAndPath,

e3a493a65972dc1197385ee746e7a129f73262f8
umidjonakramov699 <umidjonakramov699@gmail.com>
2024-04-18 10:41:37 +0500

    adddddddd

--- lib/screens/add_prodect_screen/add_product_screen.dart
+++ lib/screens/add_prodect_screen/add_product_screen.dart
@@ -1,13 +1,11 @@
 import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-
-import '../../product_data/product_data.dart';
 
 class AddProduct extends StatefulWidget {
   const AddProduct({Key? key}) : super(key: key);
@@ -26,7 +24,10 @@ class _AddProductState extends State<AddProduct> {
 
   // Function to generate a unique ID (e.g., using Firestore auto-generated ID)
   String generateUniqueId() {
-    return FirebaseFirestore.instance.collection('productsLast').doc().id;
+    return FirebaseFirestore.instance
+        .collection('productsLast')
+        .doc()
+        .id;
   }
 
   void showSnackbar(BuildContext context, String message) {
@@ -38,22 +39,58 @@ class _AddProductState extends State<AddProduct> {
     );
   }
 
-  Future<void> uploadImageToStorageAndFirestore(
-      String id,
+  // Future<void> uploadImageToFirebase(
+  //     File image, String firebaseStoragePath) async {
+  //   try {
+  //     // Firebase Storage ni olish
+  //     final storage = FirebaseStorage.instance;
+  //
+  //     // Rasmni yuklash uchun StorageReference obyektini olish
+  //     final ref = storage.ref().child(firebaseStoragePath);
+  //
+  //     // Rasmni yuklash
+  //     await ref.putFile(image);
+  //
+  //     // Yuklab olingan rasm URL sini olish
+  //     String downloadURL = await ref.getDownloadURL();
+  //     print("Rasm muvaffaqiyatli yuklandi. URL: $downloadURL");
+  //   } catch (e) {
+  //     print("Rasmni yuklashda xatolik yuz berdi: $e");
+  //   }
+  // }
+//
+
+  Future uploadImage(String path) async {
+    try {
+      if (image != null) {
+        final ref = FirebaseStorage.instance
+            .ref()
+            .child('images/${path}.png');
+        final uploadTask = ref.putFile(image!);
+        await uploadTask.whenComplete(() => null);
+        print('Image uploaded to Firebase Storage successfully.');
+      } else {
+        print('No image selected.');
+      }
+    } catch (e) {
+      print('Error uploading image: $e');
+    }
+  }
+
+  Future<void> uploadImageToStorageAndFirestore(String id,
       String imagePath,
       String productName,
       String productDescription,
       String productPrice,
-      String productCategories) async {
+      String productCategories,
+      File? image,) async {
     try {
-      Reference storageRef = FirebaseStorage.instance.ref().child(imagePath);
-      UploadTask uploadTask = storageRef.putFile(image!);
-
-      TaskSnapshot storageSnapshot = await uploadTask.whenComplete(() => null);
-      String imageUrl = await storageSnapshot.ref.getDownloadURL();
-
+      if (image == null) {
+        print('No image selected.');
+        return; // Exit function if no image is selected
+      }
       await FirebaseFirestore.instance.collection('productLast').add({
-        'id': imagePath,
+        'id': id,
         'productName': productName,
         'productDescription': productDescription,
         'imageUrl': imageUrl,
@@ -65,106 +102,119 @@ class _AddProductState extends State<AddProduct> {
       print('Image uploaded to Firebase Storage and Firestore successfully.');
     } catch (e) {
       print('Error uploading image: $e');
+      // Handle error, display error message, or notify user accordingly
     }
   }
 
+//   Future<List<String>> getImageUrlsFromFirestore(String storagePath) async {
+//     try {
+//       QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+//           .collection('productLast')
+//           .where('storagePath', isEqualTo: storagePath)
+//           .get();
+//
+//       List<String> imageUrlList = [];
+//
+//       if (querySnapshot.docs.isNotEmpty) {
+//         querySnapshot.docs.forEach((DocumentSnapshot document) {
+//           String imageUrl = document.get('imageUrl');
+//           imageUrlList.add(imageUrl);
+//         });
+//
+//         return imageUrlList;
+//       } else {
+//         print('No image URLs found for storagePath: $storagePath');
+//         return [];
+//       }
+//     } catch (e) {
+//       print('Error retrieving image URLs: $e');
+//       return [];
+//     }
+//   }
+//
+// // Method to retrieve a list of image paths from Firebase Storage
+//   Future<List<String>> getImagePathsFromStorage(String storagePath) async {
+//     try {
+//       List<String> imagePaths = [];
+//
+//       // Create a reference to the desired path in Firebase Storage
+//       Reference storageRef = FirebaseStorage.instance.ref().child(storagePath);
+//
+//       // List all items (files and directories) within the specified path
+//       ListResult listResult = await storageRef.listAll();
+//
+//       // Iterate through each item (file) in the list result
+//       listResult.items.forEach((Reference item) {
+//         // Get the full path (including parent path) of the item
+//         String fullPath = item.fullPath;
+//         // Add the full path to the list of image paths
+//         imagePaths.add(fullPath);
+//       });
+//
+//       return imagePaths;
+//     } catch (e) {
+//       print('Error retrieving image paths from Firebase Storage: $e');
+//       return []; // Return an empty list in case of error
+//     }
+//   }
+//
+//   void fetchAndPrintImagePaths(String storagePath) async {
+//     try {
+//       List<String> imagePaths = await getImagePathsFromStorage(storagePath);
+//
+//       if (imagePaths.isNotEmpty) {
+//         // Print each image path in the list
+//         print('Image Paths:');
+//         imagePaths.forEach((path) {
+//           print(path);
+//         });
+//       } else {
+//         print('No image paths found.');
+//       }
+//     } catch (e) {
+//       print('Error fetching and printing image paths: $e');
+//     }
+//   }
+//
+//   void fetchImagePaths(String storagePath) async {
+//     List<String> imagePaths = await getImagePathsFromStorage(storagePath);
+//
+//     if (imagePaths.isNotEmpty) {
+//       // Image paths retrieved successfully
+//       print('Image paths found:');
+//       imagePaths.forEach((path) {
+//         print(path);
+//       });
+//
+//       // Now you can use the retrieved image paths to download or display images
+//     } else {
+//       // No image paths found or error occurred
+//       print('No image paths found in Firebase Storage for path: $storagePath');
+//     }
+//   }
+//
+//   Future<String?> uploadImageToFirebase(XFile? image) async {
+//     if (image == null) return null;
+//     File file = File(image.path);
+//     try {
+//       String fileName =
+//           'images/${DateTime.now().millisecondsSinceEpoch}_${image.name}';
+//       await FirebaseStorage.instance.ref(fileName).putFile(file);
+//       String downloadURL =
+//       await FirebaseStorage.instance.ref(fileName).getDownloadURL();
+//       return downloadURL;
+//     } on FirebaseException catch (e) {
+//       print(e);
+//       return null;
+//     }
+//   }
 
-  Future<List<String>> getImageUrlsFromFirestore(String storagePath) async {
-    try {
-      QuerySnapshot querySnapshot = await FirebaseFirestore.instance
-          .collection('productLast')
-          .where('storagePath', isEqualTo: storagePath)
-          .get();
-
-      List<String> imageUrlList = [];
-
-      if (querySnapshot.docs.isNotEmpty) {
-        querySnapshot.docs.forEach((DocumentSnapshot document) {
-          String imageUrl = document.get('imageUrl');
-          imageUrlList.add(imageUrl);
-        });
-
-        return imageUrlList;
-      } else {
-        print('No image URLs found for storagePath: $storagePath');
-        return [];
-      }
-    } catch (e) {
-      print('Error retrieving image URLs: $e');
-      return [];
-    }
-  }
-
-
-// Method to retrieve a list of image paths from Firebase Storage
-  Future<List<String>> getImagePathsFromStorage(String storagePath) async {
-    try {
-      List<String> imagePaths = [];
-
-      // Create a reference to the desired path in Firebase Storage
-      Reference storageRef = FirebaseStorage.instance.ref().child(storagePath);
-
-      // List all items (files and directories) within the specified path
-      ListResult listResult = await storageRef.listAll();
-
-      // Iterate through each item (file) in the list result
-      listResult.items.forEach((Reference item) {
-        // Get the full path (including parent path) of the item
-        String fullPath = item.fullPath;
-        // Add the full path to the list of image paths
-        imagePaths.add(fullPath);
-      });
-
-      return imagePaths;
-    } catch (e) {
-      print('Error retrieving image paths from Firebase Storage: $e');
-      return []; // Return an empty list in case of error
-    }
-  }
-
-  void fetchAndPrintImagePaths(String storagePath) async {
-    try {
-      List<String> imagePaths = await getImagePathsFromStorage(storagePath);
-
-      if (imagePaths.isNotEmpty) {
-        // Print each image path in the list
-        print('Image Paths:');
-        imagePaths.forEach((path) {
-          print(path);
-        });
-      } else {
-        print('No image paths found.');
-      }
-    } catch (e) {
-      print('Error fetching and printing image paths: $e');
-    }
-  }
-
-  void fetchImagePaths(String storagePath) async {
-    List<String> imagePaths = await getImagePathsFromStorage(storagePath);
-
-    if (imagePaths.isNotEmpty) {
-      // Image paths retrieved successfully
-      print('Image paths found:');
-      imagePaths.forEach((path) {
-        print(path);
-      });
-
-      // Now you can use the retrieved image paths to download or display images
-    } else {
-      // No image paths found or error occurred
-      print('No image paths found in Firebase Storage for path: $storagePath');
-    }
-  }
-
-
-  @override
-  void initState() {
-    fetchAndPrintImagePaths('rasm/');
-    print('----------------------------------------------------');
-    print("---------+   ${getImagePathsFromStorage('rasm/').toString()}");
-    super.initState();
-  }
+  // void initState() {
+  //   fetchAndPrintImagePaths('rasm/');
+  //   print('----------------------------------------------------');
+  //   print("---------+   ${getImagePathsFromStorage('rasm/').toString()}");
+  //   super.initState();
+  // }
 
   //
   // // Function to add product to Firestore
@@ -214,6 +264,7 @@ class _AddProductState extends State<AddProduct> {
   //     print("Rasmni yuklashda xatolik yuz berdi: $e");
   //   }
   // }
+
 
   String selectedCategory = 'None';
 
@@ -265,8 +316,14 @@ class _AddProductState extends State<AddProduct> {
                   decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(8),
                       border: Border.all(color: Colors.black, width: 1)),
-                  height: MediaQuery.of(context).size.width / 2,
-                  width: MediaQuery.of(context).size.width / 2,
+                  height: MediaQuery
+                      .of(context)
+                      .size
+                      .width / 2,
+                  width: MediaQuery
+                      .of(context)
+                      .size
+                      .width / 2,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
@@ -275,21 +332,21 @@ class _AddProductState extends State<AddProduct> {
                         child: Expanded(
                           child: image == null
                               ? const Center(
-                                  child: Text(
-                                    'Select image',
-                                    style: TextStyle(
-                                        fontSize: 18, color: Colors.grey),
-                                  ),
-                                )
+                            child: Text(
+                              'Select image',
+                              style: TextStyle(
+                                  fontSize: 18, color: Colors.grey),
+                            ),
+                          )
                               : AspectRatio(
-                                  aspectRatio: 1,
-                                  child: Image.file(
-                                    image!,
-                                    height: 300,
-                                    width: 300,
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
+                            aspectRatio: 1,
+                            child: Image.file(
+                              image!,
+                              height: 300,
+                              width: 300,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
                         ),
                       ),
                     ],
@@ -403,13 +460,15 @@ class _AddProductState extends State<AddProduct> {
                     showSnackbar(context, 'Please enter a valid price');
                   } else {
                     var idAndPath = generateUniqueId();
+                    uploadImage(idAndPath);
                     uploadImageToStorageAndFirestore(
                         idAndPath,
                         idAndPath,
                         controllerName.text,
                         controllerDescription.text,
                         controllerPrice.text,
-                        selectedCategory);
+                        selectedCategory,
+                        image);
                     Navigator.pop(context);
                   }
                   // uploadImageToFirebase(image!,
@@ -434,15 +493,6 @@ class _AddProductState extends State<AddProduct> {
                 ),
               ),
             ),
-            // FloatingActionButton(
-            //   onPressed: () async {
-            //     // File? image = await pickImage();
-            //     addProductToFirestore();
-            //     uploadImageToFirebase(image!,
-            //         "images/${DateTime.now().millisecondsSinceEpoch}.jpg");
-            //   },
-            //   child: Text("save"),
-            // )
           ],
         ),
       ),
